3cd52233fd80a33524519f4933a4c35953f7d2ed
mdn <nevolamario@gmail.com>
2021-09-06 10:06:49 +0100

    added debounce on button press

--- WateringSystem.ino
+++ WateringSystem.ino
@@ -9,9 +9,18 @@ const int led = 13;
 
 //Variables
 int rawHumiditySensor0 = 0;
-int waterButtonState = 0;
+int waterButtonState = LOW;
+int lastWaterButtonState = LOW;
+int pumpState = HIGH;
 
 long lastTimeCheck;
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
 
 
 void setup () {
@@ -32,31 +41,51 @@ void setup () {
 
 
 
+
 void loop() {
   
   rawHumiditySensor0 = analogRead(humiditySensor0);
   
-  // read the state of the pushbutton value:
-  waterButtonState = digitalRead(waterButton);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (waterButtonState == HIGH) {
-  
-  //Turn pump on
-  digitalWrite(pump0, LOW);
-
-  } else {
-
-    // turn pump off
-    digitalWrite(pump0, HIGH);
-
-  }
-  
-  CheckTimerLed();
-  
-  
+  CheckWaterButtonPressed();
+  CheckTimerLed();  
 }
 
+
+
+
+void CheckWaterButtonPressed(){
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(waterButton);
+  
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+  // If the switch changed, due to noise or pressing:
+
+  if (reading != lastWaterButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = lmillis();
+  }
+  
+  // whatever the reading is at, it's been there for longer than the debounce
+  // delay, so take it as the actual current state:
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+ 
+    // if the button state has changed:
+    if (reading != waterButtonState) {
+      waterButtonState = reading;
+      
+      pumpState = !pumpState;  
+    }
+  }
+  
+
+  // set the pump:
+  digitalWrite(pump0, pumpState);
+
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastWaterButtonState = reading;  
+}
 
 
 void CheckTimerLed (){
